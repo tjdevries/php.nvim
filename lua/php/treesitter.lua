a0aa93704566d7f037966be511ebfd7cd426ceb4
TJ DeVries <devries.timothyj@gmail.com>
2025-02-26 10:45:02 -0700

    don't auto install

--- lua/php/treesitter.lua
+++ lua/php/treesitter.lua
@@ -1,30 +1,27 @@
-local parsers = require('nvim-treesitter.parsers')
-local parser_configs = parsers.get_parser_configs()
+local parsers = require("nvim-treesitter.parsers")
+parsers = parsers.get_parser_configs and parsers.get_parser_configs() or parsers
 
 local M = {}
 
-M.revision = "01e5550cb60ef3532ace0c6df0480f6f406113ff"
+M.revision = "47baa7ba1f9d5f436c7a72b052d2dac2166abf92"
 
 M.add_parser = function()
-  ---@diagnostic disable-next-line: inject-field
-  parser_configs.blade = {
-    install_info = {
-      url = "https://github.com/EmranMR/tree-sitter-blade",
-      files = { "src/parser.c" },
-      branch = "main",
-      revision = M.revision,
-    },
-    filetype = "blade",
-  }
+	---@diagnostic disable-next-line: inject-field
+	parsers.blade = {
+		tier = "community",
+		install_info = {
+			url = "https://github.com/EmranMR/tree-sitter-blade",
+			files = { "src/parser.c" },
+			branch = "main",
+			revision = M.revision,
+		},
+		filetype = "blade",
+	}
 end
 
 M.setup = function()
-  M.add_parser()
-
-  -- TODO: This seems like a bad plan...?
-  if not parsers.has_parser('blade') then
-    vim.cmd.TSInstall('blade')
-  end
+	M.add_parser()
+	-- vim.cmd.TSInstall("blade")
 end
 
 return M
